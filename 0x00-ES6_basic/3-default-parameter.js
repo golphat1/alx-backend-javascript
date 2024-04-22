2395429d8f59b493d87c4ca58dfb682561ef8336
golphat1 <golphat@yahoo.com>
2024-04-22 17:07:41 +0000

    condensed function

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,5 @@
+// condense the internals of a functtiion
+
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
     return initialNumber + expansion1989 + expansion2019;
 }

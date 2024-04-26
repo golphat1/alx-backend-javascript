616a8608f6a6105589237382ef4b8b50cb1bd6d2
golphat1 <golphat@yahoo.com>
2024-04-26 22:06:04 +0000

    updated tasks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,20 @@
-// Import signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js
-// 
-
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+/**
+ * Import signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js.
+ * 
+ * @param {string} firstName - The first name of the user.
+ * @param {string} lastName - The last name of the user.
+ * @param {string} fileName - The file name for the photo.
+ * @returns {Promise<Array>} - A promise that resolves to an array of objects containing status and value.
+ */
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
-    ));
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      return results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }));
+    });
 }
